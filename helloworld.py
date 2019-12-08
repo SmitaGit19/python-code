e77c86ca699561009ffb7a6e99073412e13679fd
subhasmita mishra <subhasmitamishra@subhasmitas-MacBook-Air.local>
2019-12-09 00:33:40 +0530

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,29 @@
+age = 47
+retirement = 60
+print("years to retire ", retirement - age)  # concat with comma
+
+#Variable declartion
+
+sentence ="my name is ramu"
+print(sentence)
+
+#multiiple declartion
+
+bob = 16
+sarah = 20
+mike = 36
+print(bob, sarah,mike)
+
+bob, mike, sarah = 116, 20, 36
+print(bob)
+
+#declare multiple variables with same value
+
+bob = sara = mike =17
+print(bob)
+
+#how to declare at a time string and variable
+
+name, age = "avi", 32
+print(name , age)
+
